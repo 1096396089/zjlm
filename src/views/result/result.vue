8782aea4f403d6ee4491fa6a8fdce0f843258310
Roc <1096396089@qq.com>
2025-08-14 02:54:20 +0800

    result1

--- src/views/result/result.vue
+++ src/views/result/result.vue
@@ -205,7 +205,7 @@
                     </g>
                 </g>
             </svg>
-            <Three class=" absolute -left-8  top-44"></Three>
+            <Three class=" absolute -left-8  top-[14rem]"></Three>
 
 
         </div>
