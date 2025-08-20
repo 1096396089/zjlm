7ea81ff97e9b605e2fd75ea3a3f1d1e311af596f
Roc <1096396089@qq.com>
2025-08-20 12:11:16 +0800

    delte id

--- src/views/three/three.vue
+++ src/views/three/three.vue
@@ -648,7 +648,6 @@ const switchToBTexture = (textureName: string) => {
 
 const sendData = async () => {
   const data = {
-    openId: openIdStore.openId,
     areaA: selectedATexture.value,
     areaB: selectedBTexture.value,
   }
